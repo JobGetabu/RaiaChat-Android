8cb3bc88d8a9ac181163b626653715b9e3d2a2fd
Job Getabu <getabujob@gmail.com>
2019-02-10 17:50:51 +0300

    added Constants

---
+++ app/src/main/java/com/raiachat/util/Constants.java
@@ -0,0 +1,19 @@
+package com.raiachat.util;
+
+/**
+ * Created by Job on Friday : 1/4/2019.
+ */
+public class Constants {
+    public static final String IMAGE_SAVE_CLICKS = "IMAGE_SAVE_CLICKS";
+    public static final String VIDEO_SAVE_CLICKS = "VIDEO_SAVE_CLICKS";
+
+    public static final String GBWHATAPP_PACKAGE_NAME = "com.gbwhatsapp";
+    public static final String WHATAPP_PACKAGE_NAME = "com.whatsapp";
+    public static final String WHATAPP_BUSINESS_PACKAGE_NAME = "com.whatsapp.w4b";
+
+    public static final String USER_UID = "USER_UID";
+    public static final String USER_COL = "Users";
+    public static final String IS_PRO_USER = "IS_PRO_USER";
+    public static final String REMOVE_AD_ID = "remove_ad";
+
+}
