df5fb459d6e1aa8162768ca8004a3503efa6aa77
Job Getabu <getabujob@gmail.com>
2019-02-15 06:17:26 +0300

    prototyped nav

--- app/src/main/java/com/raiachat/activities/LoginActivity.kt
+++ app/src/main/java/com/raiachat/activities/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.raiachat.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.raiachat.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlin.reflect.KClass
 
 class LoginActivity : AppCompatActivity() {
 
@@ -14,8 +18,16 @@ class LoginActivity : AppCompatActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         setContentView(R.layout.activity_login)
+
+        signinBtn.setOnClickListener { openActivity(MainActivity::class) }
+    }
+
+
+    private fun <T : Activity> openActivity(activity: KClass<T>) {
+        startActivity(Intent(this@LoginActivity, activity.java))
     }
 }
--- app/src/main/java/com/raiachat/activities/PromptLoginActivity.kt
+++ app/src/main/java/com/raiachat/activities/PromptLoginActivity.kt
@@ -1,10 +1,13 @@
 package com.raiachat.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.raiachat.R
 import com.raiachat.util.toast
 import kotlinx.android.synthetic.main.login_prompt.*
+import kotlin.reflect.KClass
 
 class PromptLoginActivity : AppCompatActivity() {
 
@@ -17,7 +20,11 @@ class PromptLoginActivity : AppCompatActivity() {
 
         loginPromptFb.setOnClickListener { toast("TODO: Fb Login :)") }
         loginPromptGoogle.setOnClickListener { toast("TODO: Google Login :)") }
-        loginPromptPhone.setOnClickListener { toast("TODO: Phone Login :)") }
+        loginPromptPhone.setOnClickListener { openActivity(LoginActivity::class) }
 
+    }
+
+    private fun <T : Activity> openActivity(activity: KClass<T>) {
+        startActivity(Intent(this@PromptLoginActivity, activity.java))
     }
 }
